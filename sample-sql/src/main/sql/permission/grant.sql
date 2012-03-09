b5fc64296ab6009e10b88c46d8f7efab6b1feb40
galaber <segolene.galabert@allianzgi.fr>
2012-03-09 15:29:41 +0100

    Harmonisation de la gestion des connexions serveur/jdbc/tokio/test-release Modification des scripts sql pour le grant

--- sample-sql/src/main/sql/permission/grant.sql
+++ sample-sql/src/main/sql/permission/grant.sql
@@ -1,35 +1,42 @@
-Print "Début Création des autorisations"
+Prompt 'Début Création des autorisations'
 
 /**************************/
 /*  Droits sur les tables */
 /**************************/
-grant select, insert, delete, update, references on PM_SEC_MODEL                    to Utilisateur, Maintenance
-grant select, insert, delete, update, references on AP_WORKFLOW_LOG                 to Utilisateur, Maintenance
-grant select, insert, delete, update, references on PM_BROADCAST_COLUMNS            to Utilisateur, Maintenance
-grant select, insert, delete, update, references on PM_BROADCAST_FILES              to Utilisateur, Maintenance
-grant select, insert, delete, update, references on PM_BROADCAST_FILE_CONTENTS      to Utilisateur, Maintenance
-grant select, insert, delete, update, references on PM_BROADCAST_SECTION            to Utilisateur, Maintenance
-grant select, insert, delete, update, references on PM_BROADCAST_SELECTOR           to Utilisateur, Maintenance
-grant select, insert, delete, update, references on PM_FIELD_IMPORT_SETTINGS        to Utilisateur, Maintenance
-grant select, insert, delete, update, references on PM_IMPORT_SETTINGS              to Utilisateur, Maintenance
-grant select, insert, delete, update, references on PM_SOURCE_SYSTEM                to Utilisateur, Maintenance
-
-grant select, insert, delete, update, references on AP_BOOK                         to Utilisateur, Maintenance
-go
+grant select, insert, delete, update, references on PM_SEC_MODEL                    to APP_USER
+/
+grant select, insert, delete, update, references on AP_WORKFLOW_LOG                 to APP_USER
+/
+grant select, insert, delete, update, references on PM_BROADCAST_COLUMNS            to APP_USER
+/
+grant select, insert, delete, update, references on PM_BROADCAST_FILES              to APP_USER
+/
+grant select, insert, delete, update, references on PM_BROADCAST_FILE_CONTENTS      to APP_USER
+/
+grant select, insert, delete, update, references on PM_BROADCAST_SECTION            to APP_USER
+/
+grant select, insert, delete, update, references on PM_BROADCAST_SELECTOR           to APP_USER
+/
+grant select, insert, delete, update, references on PM_FIELD_IMPORT_SETTINGS        to APP_USER
+/
+grant select, insert, delete, update, references on PM_IMPORT_SETTINGS              to APP_USER
+/
+grant select, insert, delete, update, references on PM_SOURCE_SYSTEM                to APP_USER
+/
+grant select, insert, delete, update, references on AP_BOOK                         to APP_USER
+/
 
 /**************************/
 /*  Droits sur les vues  */
 /**************************/
-go
 
 /**************************/
 /*  Droits sur les procs  */
 /**************************/
-go
 
 /*****************************/
 /*  Groupe Consultation (BO) */
-/*****************************/
+/****************************
 grant select, references on PM_SEC_MODEL                    to Consultation
 grant select, references on AP_WORKFLOW_LOG                 to Consultation
 grant select, references on PM_BROADCAST_COLUMNS            to Consultation
@@ -44,4 +51,7 @@ grant select, references on PM_SOURCE_SYSTEM                to Consultation
 grant select, references on AP_BOOK                         to Consultation
 go
 
-Print "Fin Création des autorisations"
+
+
+*/
+Prompt 'Fin Création des autorisations'
--- sample-sql/src/main/sql/permission/revoke.sql
+++ sample-sql/src/main/sql/permission/revoke.sql
@@ -1,26 +1,34 @@
-Print "Début de suppression des autorisations"
+Promt 'Début de suppression des autorisations'
 
 /**************************/
 /*  Droits sur les tables */
 /**************************/
-revoke all on PM_SEC_MODEL                          to Utilisateur, Maintenance, Consultation
-revoke all on AP_WORKFLOW_LOG                       to Utilisateur, Maintenance, Consultation
-revoke all on PM_BROADCAST_COLUMNS                  to Utilisateur, Maintenance, Consultation
-revoke all on PM_BROADCAST_FILE_CONTENTS            to Utilisateur, Maintenance, Consultation
-revoke all on PM_BROADCAST_FILES                    to Utilisateur, Maintenance, Consultation
-revoke all on PM_BROADCAST_SECTION                  to Utilisateur, Maintenance, Consultation
-revoke all on PM_BROADCAST_SELECTOR                 to Utilisateur, Maintenance, Consultation
-revoke all on PM_FIELD_IMPORT_SETTINGS              to Utilisateur, Maintenance, Consultation
-revoke all on PM_IMPORT_SETTINGS                    to Utilisateur, Maintenance, Consultation
-revoke all on PM_SOURCE_SYSTEM                      to Utilisateur, Maintenance, Consultation
-go
+revoke all on PM_SEC_MODEL                          from APP_USER
+/
+revoke all on AP_WORKFLOW_LOG                       from APP_USER
+/
+revoke all on PM_BROADCAST_COLUMNS                  from APP_USER
+/
+revoke all on PM_BROADCAST_FILE_CONTENTS            from APP_USER
+/
+revoke all on PM_BROADCAST_FILES                    from APP_USER
+/
+revoke all on PM_BROADCAST_SECTION                  from APP_USER
+/
+revoke all on PM_BROADCAST_SELECTOR                 from APP_USER
+/
+revoke all on PM_FIELD_IMPORT_SETTINGS              from APP_USER
+/
+revoke all on PM_IMPORT_SETTINGS                    from APP_USER
+/
+revoke all on PM_SOURCE_SYSTEM                      from APP_USER
+/
 /**************************/
 /*  Droits sur les vues  */
 /**************************/
-go
 
 /**************************/
 /*  Droits sur les procs  */
 /**************************/
-go
-Print "Fin de suppression des autorisations"
+
+Promt 'Fin de suppression des autorisations'
